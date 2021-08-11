1a71853c34afd49b2dacc580f2ebadf4b129fa43
Rohit Sharma <rohityou000@gmail.com>
2021-08-11 23:54:20 +0530

    Updates on Scheduling

--- Scripts/Word_Notifier.py
+++ Scripts/Word_Notifier.py
@@ -2,24 +2,30 @@ import time
 import requests 
 import vocabulary as vol
 from win10toast import ToastNotifier
+import schedule 
 
-# initilizing the Words 
-toaster = ToastNotifier()
-word = vol.get_word()
-meaning = None
 
-# calling Function
-try:
-    meaning = vol.get_meaning()
-except:
-    print("Please! Check your internet connection")
+def notify():
+    # initilizing the Words 
+    toaster = ToastNotifier()
+    word = vol.get_word()
+    meaning = None
 
-results = "Word :"+word.upper() +"\n" +"Meaning: "+str(meaning)
+    # calling Function
+    try:
+        meaning = vol.get_meaning()
+    except:
+        print("Please! Check your internet connection")
 
-# Creating Notification
-toaster.show_toast("\n",
-results,
-icon_path="bell-Icon.ico",
-duration=30)
+    results = "Word :"+word.upper() +"\n" +"Meaning: "+str(meaning)
 
+    # Creating Notification
+
+    toaster.show_toast("Vocabulary Builder\n",results,icon_path ="E:\github\Vocabulary-Builder\Scripts\icon.ico",duration=30)
+
+
+schedule.every(1).second.do(notify)
+while 1:
+    schedule.run_pending()
+    time.sleep(0)
 
